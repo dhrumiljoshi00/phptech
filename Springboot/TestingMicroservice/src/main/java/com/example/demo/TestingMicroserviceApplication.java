6177fa781e083e0db89bfd3e2b532e2c9df2a64e
Dhrumil <dhrumil.mca05@gmail.com>
2024-05-25 18:01:45 +0530

    update

---
+++ Springboot/TestingMicroservice/src/main/java/com/example/demo/TestingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Springboot/topsmicroservice/src/main/java/com/example/demo/TopsmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopsmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopsmicroserviceApplication.class, args);
+	}
+
+}
